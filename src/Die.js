32338301b5d53205ae8bd616e299ff1c35563b42
Jeanne <j.pietropaoli@yahoo.fr>
2023-06-18 23:12:34 +0200

    Replace die numbers by real die points in UI

--- src/Die.js
+++ src/Die.js
@@ -1,13 +1,21 @@
 import React from "react";
+import die1 from "./assets/die1.png"
+import die2 from "./assets/die2.png"
+import die3 from "./assets/die3.png"
+import die4 from "./assets/die4.png"
+import die5 from "./assets/die5.png"
+import die6 from "./assets/die6.png"
+
+const dieImages = [die1, die2, die3, die4, die5, die6]
 
 function Die(props) {
     const styles = {
-        backgroundColor : props.isHeld ? "green" : "white"
+        backgroundColor : props.isHeld ? "#59E391" : "white"
     }
 
     return(
         <div onClick={props.holdDie} style={styles} className="die">
-            <h2>{props.value}</h2>
+            <img alt="die-value" src={dieImages[props.value - 1]} />
         </div>
     )
 }
